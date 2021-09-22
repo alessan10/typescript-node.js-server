37653172f7433e337fceb8dd24412e5b14c85e26
alessan10 <alessandromauro.m@outlook.com>
2021-09-22 12:18:59 +0200

    added routes & aws-sdk

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # typescript-node.js-server
 Node.js server using typescript
+
+Technologies, libraries, framework and so on, used in this project:
+
+- typescript
+- yarn
+- node.js
+- package.json
+- nodemon vs ts-node-dev
+- express library
+- types for every library
+
+...constantly updated!
+
+Check utils.txt for instructions!
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import express from "express";
+import { homeRouter } from "./routes/homeRoutes";
 
+//EXPRESS INIT 
 const app = express();
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("Homepage");
-})
+//API ROUTING SETUP
+app.use('/home', homeRouter);
+
+app.use('/status', (_, res)=>{
+    res.status(200).send({
+        staus: 'WORKING',
+    });
+});
+interface Params{
+    a: number
+    b: number
+}
+
+type Add = (x: Params) => number;
+
+const add: Add = x => {
+    // if (b){
+    //     return a+b;
+    // } else {
+    //     return a;
+    // }
+    return x.a + x.b!;
+}
+
+console.log("res: ",add({a: 1, b: 2}));
 
 app.listen(3001, ()=> {
     console.log("started")
 })
+
+export default app;
  
